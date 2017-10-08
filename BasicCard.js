bb6d7071f78cb5287f17f8a740088ca4e6942ea9
AJama2030 <Ajama2030@gmail.com>
2017-10-07 19:58:04 -0700

    Hi

---
+++ BasicCard.js
@@ -0,0 +1,8 @@
+var BasicCard = function(front, back) {
+ this.front = front;
+ this.back = back;
+};
+
+// Exports BasicCard constructor, required in ClozeCard.js.
+module.exports = BasicCard;
+console.log("hi");
--- README.md
+++
@@ -1,2 +0,0 @@
-# Flashcard-Generator
-HW
